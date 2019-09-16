49f4f87ce16809ca19b4ced240580d29cea530fb
ryan-collingham <43373633+ryan-collingham@users.noreply.github.com>
2019-09-16 16:50:29 +0100

    Add rollup + babel config

--- testplan/web_ui/testing/src/index.js
+++ testplan/web_ui/testing/src/index.js
@@ -1,8 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import BatchReport from './Report/BatchReport';
-// import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<BatchReport />, document.getElementById('root'));
-// registerServiceWorker();
+export default BatchReport
